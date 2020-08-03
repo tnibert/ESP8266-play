74d64610f76067f9518ee7c42dfb910782ca80c2
Tim Nibert <docz2a@gmail.com>
2020-08-03 23:26:58 +1000

    how to load code onto esp8266

---
+++ micropython/loadcode.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+# these are some example
+
+# run code one time like so
+ampy -b 115200 -p /dev/ttyUSB0 run touchpad.py
+
+# load code onto system (persistent across restarts)
+ampy -b 115200 -p /dev/ttyUSB0 put main.py
