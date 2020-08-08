946db20987f3ced5cf2d6502496d52e670daffbb
Tim Nibert <docz2a@gmail.com>
2020-08-08 13:34:28 +1000

    encapsulated temperature read

--- micropython/temperature-network/readtemp.py
+++ micropython/temperature-network/readtemp.py
@@ -1,16 +1,14 @@
 """
 https://arduinomodules.info/ky-013-analog-temperature-sensor-module/
-
-https://forum.arduino.cc/index.php?topic=406890.0 -
-If you look at module faced with pins down it says S, GND, VCC but it is not actually, proper order is VCC, GND, S.
-^ todo: confirm
+On my sensor, ground and vcc seem to be switched, which is apparently common
+plugged into 3v3
 """
 from machine import Pin, ADC, I2C
 from time import sleep
 from math import log
 import ssd1306
 
-# pin constants                                                                                                                                                                              
+# pin constants
 SCL = 5
 SDA = 4
 
@@ -19,39 +17,61 @@ i2c = I2C(-1, scl=Pin(SCL), sda=Pin(SDA))
 width, height = 64, 48
 oled = ssd1306.SSD1306_I2C(width, height, i2c)
 
-# get analog input
-analog = ADC(0)
 
-# setup equation to convert resistance to temperature - steinhart-hart equation
-# steinhart-hart coefficients for thermistor
-# these coefficients are bunk, need to find correct values
-c1 = 0.001129148
-c2 = 0.000234125
-c3 = 0.0000000876741
-r1 = 10000                                                  # value of R1 on the board?
+class Temperature:
+    def __init__(self):
+        # setup equation to convert resistance to temperature - steinhart-hart equation
+        # steinhart-hart coefficients for thermistor
+        # these coefficients are bunk, need to find correct values
+        self.c1 = 0.001129148
+        self.c2 = 0.000234125
+        self.c3 = 0.0000000876741
+        self.r1 = 10000  # value of R1 on the board?
 
+        # get analog input
+        self.analog = ADC(0)
+
+        # populate
+        self.read()
+
+    def read(self):
+        self.analog_value = self.analog.read()
+        print(self.analog_value)
+
+    def convert(self):
+        # supposedly this is the steinhart-hart equation which will convert for us
+        r2 = self.r1 * (1023.0 / self.analog_value - 1.0)                   # calculate resistance on thermistor
+        logr2 = log(abs(r2))                                                # todo: remove abs
+        TK = (1.0 / (self.c1 + self.c2 * logr2 + self.c3 * logr2 * logr2 * logr2))  # temperature in Kelvin
+        TC = TK - 273.15                                                    # convert Kelvin to Celcius
+        print(TC)
+        TF = (TC * 9.0) / 5.0 + 32.0                                        # convert Celcius to Farenheit
+        return {"K": TK, "C": TC, "F": TF}
+
+    def output_oled(self, k, c, f):
+        # output raw value
+        oled.fill(0)
+        oled.text(str(self.analog_value), 0, 30, 1)
+
+        # output converted values
+        # oled.fill(0)
+        oled.text(str(int(k)) + " K", 0, 0, 1)
+        oled.text(str(int(c)) + " C", 0, 10, 1)
+        oled.text(str(int(f)) + " F", 0, 20, 1)
+
+        oled.show()
+
+    def update(self):
+        self.read()
+        # to human readable
+        hr = self.convert()
+        self.output_oled(hr["K"], hr["C"], hr["F"])
+
+
+class Network:
+    pass
+
+t = Temperature()
 while True:
-    analog_value = analog.read()
-    print(analog_value)
-
-    # output raw value
-    oled.fill(0)
-    oled.text(str(analog_value), 0, 30, 1)
-
-    # supposedly this is the steinhart-hart equation which will convert for us
-    r2 = r1 * (1023.0 / analog_value - 1.0)                 # calculate resistance on thermistor
-    logr2 = log(abs(r2))                                    # todo: remove abs
-    TK = (1.0 / (c1 + c2*logr2 + c3*logr2*logr2*logr2))     # temperature in Kelvin
-    TC = TK - 273.15                                        # convert Kelvin to Celcius
-    print(TC)
-    TF = (TC * 9.0)/ 5.0 + 32.0                             # convert Celcius to Farenheit
-
-    # output converted values
-    #oled.fill(0)
-    oled.text(str(int(TK)) + " K", 0, 0, 1)
-    oled.text(str(int(TC)) + " C", 0, 10, 1)
-    oled.text(str(int(TF)) + " F", 0, 20, 1)
-
-    oled.show()
-
+    t.update()
     sleep(0.1)
