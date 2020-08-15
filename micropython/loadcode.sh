1e72b4510b8057e0a513cd96beac6055a6d878d4
Tim Nibert <docz2a@gmail.com>
2020-08-15 17:41:37 +1000

    alter deployment step

--- micropython/loadcode.sh
+++ micropython/loadcode.sh
@@ -2,7 +2,7 @@
 # these are some example
 
 # run code one time like so
-ampy -b 115200 -p /dev/ttyUSB0 run touchpad.py
+#ampy -b 115200 -p /dev/ttyUSB0 run temperature-network/main.py
 
 # load code onto system (persistent across restarts)
-#ampy -b 115200 -p /dev/ttyUSB0 put main.py
+ampy -b 115200 -p /dev/ttyUSB0 put temperature-network/main.py
